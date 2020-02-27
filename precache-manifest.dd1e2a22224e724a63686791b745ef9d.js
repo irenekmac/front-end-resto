1730a499b3181e346c3646932acd31a455c49d47
irene macapinlac <irenekaymac@gmail.com>
2020-02-27 14:17:21 +1100

    Updates

---
+++ precache-manifest.dd1e2a22224e724a63686791b745ef9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde8abdaa3d36946163773e475c117a4",
+    "url": "/front-end-resto/index.html"
+  },
+  {
+    "revision": "0ab5faceb7adecb9398f",
+    "url": "/front-end-resto/static/css/main.e28b67d5.chunk.css"
+  },
+  {
+    "revision": "7829ce976242c441be96",
+    "url": "/front-end-resto/static/js/2.1dda9fce.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/front-end-resto/static/js/2.1dda9fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab5faceb7adecb9398f",
+    "url": "/front-end-resto/static/js/main.6638aabc.chunk.js"
+  },
+  {
+    "revision": "4e9f01dec494612a9670",
+    "url": "/front-end-resto/static/js/runtime-main.9bd799d6.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/front-end-resto/static/media/LogIn.d41d8cd9.bin"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/front-end-resto/static/media/Map.d41d8cd9.bin"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/front-end-resto/static/media/RestaurantNew.d41d8cd9.bin"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/front-end-resto/static/media/SignUp.d41d8cd9.bin"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front-end-resto/static/media/logo.5d5d9eef.svg"
+  }
+]);
