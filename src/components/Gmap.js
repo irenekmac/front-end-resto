267fd8795b7558fa1dba4b3f9b794f1c310ad61e
irene macapinlac <irenekaymac@gmail.com>
2020-02-28 00:13:02 +1100

    edits to gmaps api

--- src/components/Gmap.js
+++ src/components/Gmap.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
-import Button from './Button';
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyCSodW9rAze3SoIql56Y6CE4Z51iMUcXKw");
 const API_KEY = "AIzaSyCSodW9rAze3SoIql56Y6CE4Z51iMUcXKw"
 
 const StyledMap = styled.div`
+
 
   border: solid #CCC 1px;
   width: 50%;
@@ -15,12 +18,17 @@ const StyledMap = styled.div`
 `
 
 class Gmap extends React.Component {
-
-    state = {
-      address: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: null,
+      latitude: null,
+      longitude: null
     };
 
-    getRestoLocation = () => {
+  }
+
+    getRestoAddress = () => {
      let url = 'https://team-hangry.herokuapp.com/restaurants'
 
      axios.get(url)
@@ -35,18 +43,79 @@ class Gmap extends React.Component {
   }
 
     componentDidMount(){
-      this.getRestoLocation();
+      this.getRestoAddress();
       console.log(this.props);
     }
+    getAddressCoords = () => {
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.address}&key=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ latitude: data.geometry.location.latitude, longitude: data.geometry.location.longitude })
+        })
+        .catch(error => console.warn(error))
+    }
+
+    getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(this.getCoordinates, this.handleLocationError);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+  getCoordinates = (position) => {
+    // console.log(position.coords.latitude);
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    })
+  }
+
+  // reverseGeoCoordinates = () => {
+  //   fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.state.latitude},${this.state.longitude}&sensor=false&key=${API_KEY}`)
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  //     .catch(error => console.warn(error))
+  //
+  // }
+
+  handleLocationError = (error) => {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.")
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.")
+        break;
+        default:
+        alert("An unknown error occurred.")
+      }
+    }
+
 
   render () {
     console.warn("test", this.state.address)
 
     return (
       <div>
-      <h1>Address: {this.state.address}!</h1>
+      <h1>Where u?</h1>
 
-      <iframe width="100%" height="450" src="https://www.google.com/maps/embed/v1/place?q={{{{LAT}}}},{{{{{{LONG}}}}}&amp;key=AIzaSyCSodW9rAze3SoIql56Y6CE4Z51iMUcXKw"></iframe>
+
+      <button onClick={this.getLocation}>Where am i?</button>
+
+      <p>Address: {this.state.address}!</p>
+      {
+        this.state.latitude && this.state.longitude ?
+        <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.latitude},${this.state.longitude}&zoom=16&size=500x400&sensor=false&markers=color:red%7C${this.state.latitude},${this.state.longitude}}&key=${API_KEY}`} alt='' />
+        :
+        null
+      }
 
       </div>
     );
